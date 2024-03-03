fdd7112c362bc1c6abc4dcf49d41438ef4c20877
josh-dp <dprentice@ufl.edu>
2024-03-03 15:52:26 -0500

    -barebones user page revamp

--- src/app/(main)/user/page.tsx
+++ src/app/(main)/user/page.tsx
@@ -1,13 +1,17 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
 import { api } from "@/trpc/server";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import VerifyStudentButton from "@/components/buttons/verifyStudentButton";
 import Link from "next/link";
+
+const cardStyle = {
+  width: "580px",
+  padding: 10,
+  borderRadius: 10,
+  border: "4px solid #e6b3ff",
+  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+};
 
 export default async function UserPage() {
   const user = await api.users.getCurrent.query();
@@ -15,47 +19,132 @@ export default async function UserPage() {
   if (!user) return null;
 
   return (
-    <div className="flex w-full justify-center ">
-      <Card className="mt-12 w-full max-w-xl">
-        <CardHeader>
-          <CardTitle>
-            {user.firstName} {user.lastName}
-          </CardTitle>
-          <CardDescription>{user.email}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex items-center">
-          <p className="pr-1">Skills:</p>
-          {user.skills.map((skill, index) => (
+    <>
+      <div className="flex items-start justify-center">
+        <div className="flex-col">
+          <div className="mt-4 flex h-48 w-80 flex-col items-center justify-center border-2">
+            <Avatar className="size-20 hover:cursor-pointer">
+              <AvatarImage src="https://wallpapers.com/images/high/funny-profile-picture-7k1legjukiz1lju7.webp" />
+              <AvatarFallback>Peer</AvatarFallback>
+            </Avatar>
             <p
-              key={index}
-              className="w-min rounded-full border border-border px-2 py-0.5"
+              className="mt-2"
+              style={{
+                fontSize: "1.75rem",
+                fontWeight: "bold",
+              }}
             >
-              {skill}
+              {user.firstName} {user.lastName}
             </p>
-          ))}
-        </CardContent>
-        <CardContent>
-          <p>Bio: {user.bio}</p>
-        </CardContent>
-        <div className="flex">
-          <CardContent>
-            <p>Github: {user.github}</p>
-          </CardContent>
-          <CardContent>
-            <p>Linkedin: {user.linkedin}</p>
-          </CardContent>
-          <CardContent>
-            <p>Website: {user.website}</p>
-          </CardContent>
+            <p
+              style={{
+                fontSize: "1rem",
+                color: "gray",
+              }}
+            >
+              {user.username}
+            </p>
+            <p
+              style={{
+                fontSize: "1.01rem",
+              }}
+            >
+              Verification Status:{" "}
+              {user.isVerifiedStudent ? "Verified" : "Not Verified"}
+            </p>
+          </div>
+          <div className="w-80 border-2 px-5 py-2">
+            <p>{user.bio}</p>
+          </div>
+          <div className="flex w-80 flex-col items-center justify-center border-2 py-2">
+            <b style={{ fontSize: "1.25rem" }}>Links</b>
+            <div style={{ fontSize: "0.8rem" }}>
+              {!user.github && !user.linkedin && !user.website ? (
+                <div className="mt-2">
+                  This user has not provided any personal links.
+                </div>
+              ) : null}
+              <div className="mt-2 flex">
+                {user.github && (
+                  <a
+                    style={{ textDecoration: "underline", marginRight: "10px" }}
+                    href={user.github}
+                  >
+                    GitHub
+                  </a>
+                )}
+                {user.linkedin && (
+                  <a
+                    style={{ textDecoration: "underline", marginRight: "10px" }}
+                    href={user.linkedin}
+                  >
+                    LinkedIn
+                  </a>
+                )}
+                {user.website && (
+                  <a
+                    style={{ textDecoration: "underline", marginRight: "10px" }}
+                    href={user.website}
+                  >
+                    Personal Website
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 flex w-80 justify-center py-2">
+            <Link href="/user/edit">
+              <Button variant="default">Edit Profile</Button>
+            </Link>
+          </div>
+
+          {!user.isVerifiedStudent && (
+            <div className="mt-2 flex w-80 justify-center py-2">
+              <VerifyStudentButton />
+            </div>
+          )}
         </div>
-        <CardContent className="flex justify-center">
-          <Link href="/user/edit">
-            <Button variant="default" className="my-4">
-              Edit Profile
-            </Button>
-          </Link>
-        </CardContent>
-      </Card>
-    </div>
+
+        <div className="ml-20 mt-4 flex-col">
+          <div>
+            <Card style={cardStyle}>
+              <CardHeader
+                className="text-center"
+                style={{
+                  padding: 7,
+                  fontSize: "1.25rem",
+                  fontWeight: "bold",
+                  color: "purple",
+                }}
+              >
+                Organizations
+              </CardHeader>
+              <CardContent className="text-center">
+                This user is not part of any organizations.
+              </CardContent>
+            </Card>
+          </div>
+
+          <div>
+            <Card className="mt-3" style={cardStyle}>
+              <CardHeader
+                className="text-center"
+                style={{
+                  padding: 7,
+                  fontSize: "1.25rem",
+                  fontWeight: "bold",
+                  color: "purple",
+                }}
+              >
+                Events
+              </CardHeader>
+              <CardContent className="text-center">
+                This user is not registered for any events.
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
