4038bed79ba093f917e926b2b58359a406eb42d7
Courtney Leu <courtneygleu@outlook.com>
2024-03-16 22:48:08 -0400

    edits to org validator

--- src/lib/validators/Organization.ts
+++ src/lib/validators/Organization.ts
@@ -4,7 +4,6 @@ import { OrganizationType } from "@prisma/client";
 export const newOrgSchema = z.object({
   name: z.string().min(1),
   email: z.string().min(1).optional(),
-  president: z.string().min(1),
   type: z.nativeEnum(OrganizationType),
   description: z.string().min(50),
   image: z.string().url().optional(),
