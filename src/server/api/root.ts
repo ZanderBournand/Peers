7183f81788834b9c80ba3b4dec438705fb503bc0
Courtney Leu <courtneygleu@outlook.com>
2024-03-22 17:26:48 -0400

    Add new org router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,12 @@
 import { userRouter } from "@/server/api/routers/users";
 import { eventRouter } from "@/server/api/routers/events";
+import { organizationRouter } from "@/server/api/routers/organizations";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
   users: userRouter,
   events: eventRouter,
+  organizations: organizationRouter,
 });
 
 export type AppRouter = typeof appRouter;
