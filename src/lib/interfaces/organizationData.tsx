b382e8ce8c165bb1f309c528eb61f2b74855fb7f
Courtney Leu <courtneygleu@outlook.com>
2024-03-25 23:57:56 -0400

    lib/interfaces addition

--- src/lib/interfaces/organizationData.tsx
+++ src/lib/interfaces/organizationData.tsx
@@ -1,4 +1,11 @@
 export interface OrganizationData {
-  id: string;
-  name: string;
-}
+    id: string;
+    name: string;
+    description: string;
+    email?: string | null;
+    type: string;
+    image?: string | null;
+    instagram?: string | null;
+    facebook?: string | null;
+    discord?: string | null;
+  }
