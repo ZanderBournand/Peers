ab6e4773029268de3982dbc073bfc6f2f42d72b9
Zander Bournand <z.bournand@gmail.com>
2024-03-27 00:29:40 -0400

    prettier fixes

--- src/components/user/verifyStudentButton.tsx
+++ src/components/user/verifyStudentButton.tsx
@@ -12,7 +12,7 @@ const VerifyStudentButton: React.FC<object> = () => {
       onClick={verificationAlert?.openAlert}
       className="bg-green-200/20"
     >
-      <CheckBadgeIcon className="mr-2 h-6 w-6" color="green"/>
+      <CheckBadgeIcon className="mr-2 h-6 w-6" color="green" />
       Verify Your Student Status!
     </Button>
   );
