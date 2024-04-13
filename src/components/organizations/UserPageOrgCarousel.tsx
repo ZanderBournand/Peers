83615744d9e4dd1df1428b87c17b24d9adb841c1
Zander Bournand <z.bournand@gmail.com>
2024-04-13 19:03:08 -0400

    slight changes to user page carousels

--- src/components/organizations/UserPageOrgCarousel.tsx
+++ src/components/organizations/UserPageOrgCarousel.tsx
@@ -25,12 +25,12 @@ export default function UserPageOrganizationCarousel({
     <Carousel className="my-2 ml-6 w-11/12 items-center justify-center">
       <CarouselContent className="mx-2 mt-2">
         {organizations.map((organization) => (
-          <CarouselItem key={organization.id} className="mr-4 basis-2/5 pl-4">
+          <CarouselItem key={organization.id} className="mr-4 basis-1/2 pl-4">
             <Link
               href={`/organization/${organization.id}`}
-              className="flex flex-row items-center"
+              className="flex flex-row items-center transition-all duration-200 hover:scale-105"
             >
-              <div className="relative flex aspect-square h-12 items-center justify-center ">
+              <div className="relative flex aspect-square h-[75px] items-center justify-center ">
                 <Image
                   src={organization.image ?? ""}
                   alt="host image"
@@ -42,23 +42,27 @@ export default function UserPageOrganizationCarousel({
                   className="rounded-lg transition-opacity duration-500 group-hover:opacity-70"
                 />
               </div>
-              <div className="flex-start ml-2 flex flex-col">
-                <div className="flex flex-row items-center">
-                  <p className="text-sm font-semibold">{organization.name}</p>
+              <div className="flex-start ml-3 flex flex-col">
+                <div className="flex h-10 flex-row items-center">
+                  <p className="line-clamp-2 text-sm font-semibold">
+                    {organization.name}
+                  </p>
+                </div>
+                <div className="mb-2 mt-2 flex flex-row items-center">
                   <Image
                     src={organization?.university?.logo ?? ""}
                     alt="selected image"
-                    width={15}
-                    height={15}
+                    width={20}
+                    height={20}
                     style={{
                       objectFit: "cover",
                     }}
-                    className="ml-2 rounded transition-opacity duration-500 group-hover:opacity-70"
+                    className="mr-2 rounded transition-opacity duration-500 group-hover:opacity-70"
                   />
-                </div>
-                <div className="mt-2 flex w-max flex-row items-center rounded-sm bg-purple-100/30 px-2 py-0.5 text-xs text-slate-600">
-                  <AcademicCapIcon className="mr-1 h-5 w-5" />
-                  {formatEnumName(organization.type)}
+                  <div className="flex w-max flex-row items-center rounded-sm bg-purple-100/30 px-2 py-0.5 text-xs text-slate-600">
+                    <AcademicCapIcon className="mr-1 h-5 w-5" />
+                    {formatEnumName(organization.type)}
+                  </div>
                 </div>
               </div>
             </Link>
